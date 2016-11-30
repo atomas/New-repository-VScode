d685f7550a1351734546560ac9a7cc40b10c28b1
Anto Tomas from VS15 <at@ekobit>
2016-11-30 12:32:22 +0100

    new class VS15

---
+++ Console app for Github/ClassVS15.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_app_for_Github
+{
+    class ClassVS15
+    {
+    }
+}
