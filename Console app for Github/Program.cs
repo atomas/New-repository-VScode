f3f51aaaac3cc7a4625b0dd54bba7d7ff6affbce
Anto Tomas <at@ekobit.corp>
2016-11-30 09:49:46 +0100

    VS code - 1. commit

---
+++ Console app for Github/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_app_for_Github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //todo: some code
+        }
+    }
+}
