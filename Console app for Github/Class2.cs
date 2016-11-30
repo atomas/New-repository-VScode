c41de1573e1861ed007b16dafb5a9b03d8bc46d5
Anto Tomas from VS15 <at@ekobit>
2016-11-30 10:55:33 +0100

    commit Branch2 Class2 method

--- Console app for Github/Class2.cs
+++ Console app for Github/Class2.cs
@@ -2,4 +2,10 @@
 
 class Class2{
     //TODO: something
+
+    private int number1{get; set;}
+    public int methodClass2(int number1){
+        int number2 = number1;
+        return number2;
+    }
 }
